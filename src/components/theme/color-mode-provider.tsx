8c82543bda81d6d71aab0b2e17853d87513c56c3
flakesnoy <abelchstr@gmail.com>
2026-08-28 15:37:30 +0200

    feat: update files

---
+++ src/components/theme/color-mode-provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ThemeProvider, type ThemeProviderProps } from "next-themes";
+
+export function ColorModeProvider(props: ThemeProviderProps) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      forcedTheme="dark"
+      defaultTheme="dark"
+      disableTransitionOnChange
+      {...props}
+    />
+  );
+}

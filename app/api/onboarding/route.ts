3b10527225560e3610501092476a458237df8b2c
flakesnoy <abelchstr@gmail.com>
2026-08-30 18:54:38 +0200

    chore: addition of an onboarding route

---
+++ app/api/onboarding/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ message: "GET request received" });
+}
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  return NextResponse.json({ message: "POST request received", data });
+}
